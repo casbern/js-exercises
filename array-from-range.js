d86332fd15787848a67f9fe87cb804d7f5c9dca2
Cássia <cascrisbern@gmail.com>
2020-01-24 18:41:04 -0300

    exercise using array

--- array-from-range.js
+++ array-from-range.js
@@ -2,8 +2,9 @@ function arrayFromRange(min, max) {
   let output = []
 
   for(let i = 0; i <= max; i++) {
-    
+    console.log(`before ${output}`)
       output.push(i + 1)
+      console.log(`after ${output}`)
     
     return output
   }
