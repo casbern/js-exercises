a426d7f8c1684b73788026d67b3400480b1e6088
Cássia <cascrisbern@gmail.com>
2020-01-26 10:53:06 -0300

    exercise using objects

--- price-range-object.js
+++ price-range-object.js
@@ -1,16 +1,62 @@
-const priceRanges = [
-  {label: "$", tooltip: "Inexpensive", minPerPerson: 0, maxPerPerson: 10},
-  {label: "$$", tooltip: "Moderate", minPerPerson: 11, maxPerPerson: 20},
-  {label: "$$$", tooltip: "Expensive", minPerPerson: 21, maxPerPerson: 50},
+const priceRanges = [{
+    label: "$",
+    tooltip: "Inexpensive",
+    minPerPerson: 0,
+    maxPerPerson: 10
+  },
+  {
+    label: "$$",
+    tooltip: "Moderate",
+    minPerPerson: 11,
+    maxPerPerson: 20
+  },
+  {
+    label: "$$$",
+    tooltip: "Expensive",
+    minPerPerson: 21,
+    maxPerPerson: 50
+  },
 ];
 
-let restaurants = [
-  {name: "Cookingood", averagePerPerson: 5},
-  {name: "Delicias da Vovo", averagePerPerson: 50},
-  {name: "Steak House", averagePerPerson: 15},
+let restaurants = [{
+    name: "Cookingood",
+    averagePerPerson: 5
+  },
+  {
+    name: "Art de Cozinhar",
+    averagePerPerson: 9
+  },
+  {
+    name: "Amor de Cozinha",
+    averagePerPerson: 2
+  },
+  {
+    name: "Delicias da Vovo",
+    averagePerPerson: 50
+  },
+  {
+    name: "Steak House",
+    averagePerPerson: 15
+  },
 
 ]
 
-function restaurantsInsideBuget(priceRange, restaurants) {
+let insideBuget = []
 
+function restaurantsInsideBuget(priceRange, ...restaurants) {
+
+  for (restaurant of restaurants) {
+
+    if (restaurant.averagePerPerson >= priceRange.minPerPerson && 
+      restaurant.averagePerPerson <= priceRange.maxPerPerson) {
+
+      insideBuget.push(restaurant)
+
+    } 
+  }
+  return insideBuget
 }
+
+const finalRestaurants = restaurantsInsideBuget(priceRanges[0], ...restaurants)
+
+console.log(finalRestaurants)
