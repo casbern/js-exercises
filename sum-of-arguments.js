d79384f4330976383304a4322493122c3c865694
Cássia <cascrisbern@gmail.com>
2020-02-04 10:29:32 -0300

    solution using reduce

--- sum-of-arguments.js
+++ sum-of-arguments.js
@@ -30,12 +30,14 @@
 // ===== ******* =====
 
 function sumOfArguments(...items) {
+  console.log(items) // items will become an array. when received an array, items will become an array of array.
+  
   if (items.length === 1 && Array.isArray(items[0])) {
-    items = [...items[0]]
+    items = [...items[0]] // you set another value to items variable (spread the first array)
   }
 
   return items.reduce((acc, initialV) => acc += initialV)
 }
 
-console.log(sumOfArguments(1, 2, 3, 4, 5))
+//console.log(sumOfArguments(1, 2, 3, 4, 5))
 console.log(sumOfArguments([1, 2, 3, 4, 5]))
