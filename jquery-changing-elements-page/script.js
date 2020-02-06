010d5d3a60bd0d23fe5816a098be2f123d9b81d7
Cássia <cascrisbern@gmail.com>
2020-02-06 19:07:38 -0300

    exercise jquery to change elements

--- jquery-changing-elements-page/script.js
+++ jquery-changing-elements-page/script.js
@@ -8,14 +8,25 @@ $(document).ready(function() {
 
     if(hasImage) {
       //remove image
+      console.log('click to show')
+
       $('button').css('background-color', 'darkgray')
       $('img').addClass('hide')
+
+      hasImage = false
+
     } else {
       //add image
+      console.log('click to hide')
+
       $('button').css('background-color', 'brown')
       $('img').removeClass('hide')
+
+      hasImage = true
+
     }
     
+    console.log(hasImage)
     
   })
 
