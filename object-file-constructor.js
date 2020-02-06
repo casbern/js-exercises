b3fedffe0d3a13dea496929025ecc7e3c62984a8
Cássia <cascrisbern@gmail.com>
2020-02-06 15:29:21 -0300

    exercise using contructor function

--- object-file-constructor.js
+++ object-file-constructor.js
@@ -1,20 +1,43 @@
+//https://github.com/choosy/js-homework/blob/master/objects-file-constructor.txt
+
+const fs = require('fs')
+
+
 function File (filePath) {
+
   this.filePath = filePath
+
   this.getSize = function getSize() {
-    //return size
+    const takingFile = this.filePath.split('/').pop()
+    return fs.statSync(takingFile).size
   }
+
   this.isFile = function isFile() {
-    //return boolean
+    return fs.existsSync(this.filePath)
   }
+
   this.fileType = function fileType() {
-    //return a string with the extention
+    return this.filePath.split('/').pop().split('.').pop()
   }
+
   this.moveTo = function moveTo(pathName) {
-   // return 
+   return fs.rename(this.filePath, pathName, function(err) {
+     if (err) {
+       throw err
+     }
+   })
   }
 
 }
 
-const myFile = new File("/Users/cah/js-training/exercise.js")
+const myFile = new File("/Users/cassiabernardo/Dev/js-training/objects.js")
 
-console.log(myFile.filePath)
+//console.log(myFile.filePath)
+
+//console.log(myFile.getSize())
+
+//console.log(myFile.isFile())
+
+//console.log(myFile.fileType())
+
+//console.log(myFile.moveTo("/Users/cassiabernardo/Dev"))
