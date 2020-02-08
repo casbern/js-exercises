b45bd0c867131356f82b3cbe238fb9a253725a94
Cássia <cascrisbern@gmail.com>
2020-02-07 21:12:52 -0300

    exercise ajax and ipis

---
+++ ajax-and-ipis/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+  $('button').on('click', function() {
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://api.github.com/users/cah90", true);
+    xhr.send(null);
+    
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            const result = JSON.parse(xhr.responseText)
+            $('p').text(result)
+            console.log(result)
+        }
+      }
+
+    })
+})
+
+
+
