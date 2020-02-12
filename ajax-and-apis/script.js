33878400c9c505cc5eb63357bcd0cdf6bfba87d0
Cássia <cascrisbern@gmail.com>
2020-02-12 11:51:02 -0300

    displaying the result after the click

--- ajax-and-apis/script.js
+++ ajax-and-apis/script.js
@@ -3,16 +3,21 @@ $(document).ready(function() {
   $('button').on('click', function() {
 
     var xhr = new XMLHttpRequest();
-    console.log(xhr)
     xhr.open("GET", "https://api.github.com/users/cah90", true);
     xhr.send(null);
     
     xhr.onload = function() {
         if (xhr.status === 200) {
-            const result = xhr.responseText
-            const result = JSON.parse(xhr.responseText) 
-            $('p').text(result)
-            console.log(result)
+            //const result = xhr.responseText //it return a js string
+            const result = JSON.parse(xhr.responseText) //it returns a js object
+
+            $('.login').text(result.login) //shows the value of the property
+            $('.id').text(result.id) 
+            
+
+            for(key in result) {
+              console.log(key,result[key]) //access the key and the value
+            }
         }
       }
 
