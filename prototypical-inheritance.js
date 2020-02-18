4bcb3fb5b57763223ef738bc80bf4792508d1520
Cássia <cascrisbern@gmail.com>
2020-02-17 21:14:06 -0300

    prototypical inheritance example

--- prototypical-inheritance.js
+++ prototypical-inheritance.js
@@ -1,12 +1,14 @@
 // Creating you own prototypical inheritance
 
 function Shape () {
-
 }
 
 Shape.prototype.duplicate = function () {
   console.log("duplicate")
 }
+
+Object.create(Shape.prototype)
+Circle.prototype = Object.create(Shape.prototype)
 
 function Circle (radius) {
   this.radius = radius
@@ -16,8 +18,6 @@ Circle.prototype.draw = function() {
   console.log("draw")
 }
 
-Object.create(Shape.prototype)
-Circle.prototype = Object.create(Shape.prototype)
 
 const shape = new Shape()
 const circle = new Circle(1)
