62fbfec67e9b92b6e1902f8663f026b3b1b610e9
Cássia <cascrisbern@gmail.com>
2020-03-08 19:35:44 -0300

    constructor exercise

--- objects-constructor-routes.js
+++ objects-constructor-routes.js
@@ -1,40 +1,29 @@
-function splitRequest(request) {
-  const method = request.split(" ")
-  return method
-  
-}
-
-function showMethod(request) {
-  const method = splitRequest(request)
-   return method[0]
-}
-
-function showProtocol(request) {
-  const protocol = splitRequest(request)
-  const stringTransform = protocol[1].split("/", 1)
-  return stringTransform.toString()
-}
-
-function showHostName(request) {
-  const hostName = splitRequest(request)
-  const stringTransform = hostName[1].split("/")
-  return stringTransform[2].toString() 
-}
-
-function showURL(request) {
-  const url = splitRequest(request)
-  return url[1].substr(19)
-}
-
-
 function Route(request) {
-  this.method = showMethod(request)
-  this.protocol = showProtocol(request)
-  this.hostName = showHostName(request)
-  this.url = showURL(request)
 
+  
+  
+  function showProtocol() {
+    const stringTransform = url.split("/", 1)
+    return stringTransform.toString()
+  }
+  
+  function showHostName() {
+    const stringTransform = url.split("/")
+    return stringTransform[2].toString()
+  }
+  
+  function showURL() {
+    return url.substr(19)
+  }
+  
+  let  [ method, url ] = request.split(" ")
+
+  this.method = method
+  this.protocol = showProtocol()
+  this.hostName = showHostName()
+  this.urlPath = showURL()
+  
 }
 
 const route = new Route("POST https://example.com/teachers/create")
 console.log(route)
-
