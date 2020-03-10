9b61112861645ae211cc57c914c7db47a73e861c
Cássia <cascrisbern@gmail.com>
2020-03-10 09:38:30 -0300

    exercising abstraction

--- objects-constructor-person.js
+++ objects-constructor-person.js
@@ -1,29 +1,31 @@
- function isValidName (name) {
-  if (typeof (name) == 'string') {
-    return removeSpaces(name)
-  } else {
-    throw "Invalid name"
-  }
-}
-
-function removeSpaces(name) {
-  return name.trim(" ")
-}
-
-function isValidAge(age) {
-  if(age > 0 && age < 160) {
-    return age
-  } else {
-    throw "Invalid age"
-  } 
-}
 
 function Person(firstName, lastName, age) {
-  this.firstName = isValidName(firstName)
-  this.lastName = isValidName(lastName)
+  this.name = `${isValidName(firstName)} ${isValidName(lastName)}`
   this.age = isValidAge(age)
+
+  function isValidName (name) {
+    if (typeof (name) == 'string') {
+      return removeSpaces(name)
+    } else {
+      throw "Invalid name"
+    }
+  }
+  
+  function removeSpaces(name) {
+    return name.trim(" ")
+  }
+  
+  function isValidAge(age) {
+    if(age > 0 && age < 160) {
+      return age
+    } else {
+      throw "Invalid age"
+    } 
+  }
 }
 
 const person = new Person("Monica", "Silva", 30)
 const person1 = new Person("John", "Doe", 29)
 const person2 = new Person("Denis", "Rogrigues", 10)
+
+console.log(person)
