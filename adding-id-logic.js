ad82a03259dadffed377d32d99b50755c0f868c9
Cássia <cascrisbern@gmail.com>
2020-03-13 16:02:06 -0300

    logic for adding ids

---
+++ adding-id-logic.js
@@ -0,0 +1,14 @@
+const fruits = []
+
+
+const lastFruit = fruits[fruits.length-1]
+console.log(lastFruit)
+
+
+if(lastFruit) {
+  id = fruits.push("pineapple")
+}
+
+id = fruits.push("strawberry") 
+
+console.log(fruits)
