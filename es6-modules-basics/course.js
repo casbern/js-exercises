482378d4a1e8eac9e5805fa9e8e5388fb82a5f8b
Cássia <cascrisbern@gmail.com>
2020-03-18 15:31:49 -0300

    exercise with import

---
+++ es6-modules-basics/course.js
@@ -0,0 +1,8 @@
+function Course(title, description, price, keywords) {
+  this.title = title
+  this.description = description
+  this.price = price
+  this.keywords = keywords
+}
+
+export {Course}
---
+++ es6-modules-basics/script.js
@@ -0,0 +1,9 @@
+import {Course} from "./course.js"
+
+const c = new Course("Learn more JS", "Learn JS fast in a nice and fun way", "$40,99", ["JS", "fun"])
+const d = new Course("Physical Exercises", "Be healthier with us!", "$99,99", ["push-ups", "happiness"])
+const e = new Course("Intensive Bootcamp", "Learn how to code fast", "$240,50", ["HTML", "CSS", "JS"])
+
+console.log(c)
+console.log(d)
+console.log(e)
