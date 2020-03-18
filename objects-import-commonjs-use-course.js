aadfb6184909b3d451ded80b85eeb54300da1ae6
Cássia <cascrisbern@gmail.com>
2020-03-18 15:03:59 -0300

    exercise with import

--- objects-import-commonjs-use-course.js
+++ objects-import-commonjs-use-course.js
@@ -1,12 +1,15 @@
-const {Course} = require("./objects-import-commonjs-course2.js")
+//const {Course} = require("./objects-import-commonjs-course2.js")
+//* It was used exports.something = something. It exports as an object.
+//* It was used the destructor, so I have just the function.
 
-console.log(Course)
-//It was used the destructor, so I have just the function.
+const Course = require("./objects-import-commonjs-course2.js")
+//* It was used module.exports = something.
+//* This one does not export as an object.
 
-// const c = new Course("Learn more JS", "Learn JS fast in a nice and fun way", "$40,99", ["JS", "fun"])
-// const d = new Course("Physical Exercises", "Be healthier with us!", "$99,99", ["push-ups", "happiness"])
-// const e = new Course("Intensive Bootcamp", "Learn how to code fast", "$240,50", ["HTML", "CSS", "JS"])
+const c = new Course("Learn more JS", "Learn JS fast in a nice and fun way", "$40,99", ["JS", "fun"])
+const d = new Course("Physical Exercises", "Be healthier with us!", "$99,99", ["push-ups", "happiness"])
+const e = new Course("Intensive Bootcamp", "Learn how to code fast", "$240,50", ["HTML", "CSS", "JS"])
 
-// console.log(c)
-// console.log(d)
-// console.log(e)
+console.log(c)
+console.log(d)
+console.log(e)
