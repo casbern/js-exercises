8f43e1c3aacc00b5be8770a4cd8db31276ab6802
Cássia <cascrisbern@gmail.com>
2020-03-20 09:28:15 -0300

    corrections on the ensino function

--- ensino-function.js
+++ ensino-function.js
@@ -1,21 +1,18 @@
 function grade(schoolYear) {
-  const splitYear = schoolYear.split("")
+  const [year, notNeeded, letter] = schoolYear.split("")
 
-  splitYear.find(function(letter) {
     if(letter == "F") {
-      console.log(`${splitYear[0]}º Ano do Ensino Fundamental`)
+      return `${year}º Ano do Ensino Fundamental`
     } else if (letter == "M") {
-      console.log(`${splitYear[0]}º Ano do Ensino Médio`)
+      return `${year}º Ano do Ensino Médio`
     } else {
       return false
     }
-  })
+
 }
 
- 
 
-grade("1EF")
-grade("2EF")
-grade("9EF")
-grade("1EM")
-grade("2EM")
+const teste = grade("9EF")
+console.log(teste)
+
+console.log(grade("2EF"))
