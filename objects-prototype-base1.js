86264463f30c5907ae6dbeba857dd66191316035
Cássia <cascrisbern@gmail.com>
2020-04-16 10:03:05 -0300

    exercise with prototypes

--- objects-prototype-base1.js
+++ objects-prototype-base1.js
@@ -10,12 +10,8 @@ const course2 = {
   price: 20
 }
 
-//! How to make it???
-//! parent1 make it to be the prototype (parent) of course1
-//! parent2 make it to be the prototype (parent) of course2
-
-const parent1 = {}
-const parent2 = {}
+const parent1 = Object.setPrototypeOf(course1)
+const parent2 = Object.setPrototypeOf(course2)
 
 console.log('parent1')
 console.log(parent1)
@@ -28,9 +24,6 @@ console.log(parent1 === parent2)
 
 console.log(Object.getPrototypeOf(parent1) === Object.getPrototypeOf(parent2))
 
-//! This will prove who is parent1, who is parent2 and if they are equals.
-//! Think of the signification of parent1/parent2. Who is it?
-
 //* the object parent1 and parent2 are not equal. Each has its own address
 //* in memory that is stored in a variable and when compared, those references are not the same.
 
@@ -42,11 +35,11 @@ console.log(Object.getPrototypeOf(parent1) === Object.getPrototypeOf(parent2))
 
 function FancyCourse(course) {
   this.toString = function() {
-    return course.title
+    return this.title
   }
 
   this.convertPrice = function() {
-    const usdPrice = course.price
+    const usdPrice = this.price
     const realPrice = 5.24
     const result = usdPrice * realPrice
     return result.toFixed(2)
