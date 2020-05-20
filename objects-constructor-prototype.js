7c91f69b71aacaa9a191e4ce262c4b79051ec2bd
Cássia <cascrisbern@gmail.com>
2020-05-20 12:39:20 -0300

    exercise with property descriptors in JS

---
+++ objects-constructor-prototype.js
@@ -0,0 +1,38 @@
+const person = {
+  name: "Paulo",
+  age: 20
+}
+
+const my_arr = [
+  {
+    name: "Ieda",
+    age: 52
+  }
+]
+
+function Course() {
+  this.name = "Node",
+  this.sayCourse = function() {
+    return this.name
+  }
+}
+
+const course = new Course()
+
+// ======
+
+const personProto = Object.getPrototypeOf(person) //or person.__proto__
+const personObjProto = Object.prototype
+
+const my_arrProto = Object.getPrototypeOf(my_arr)
+const arrayObjProto = Array.prototype
+
+const courseProto = Object.getPrototypeOf(course)
+const courseObjProto = Course.prototype
+
+//A literal object has a prototype that is its parent.
+//This literal object is created using the object constructor function. This constructor
+//also has a protype property and this prototype property is used as the prototype for all
+//objects created by this constructor.
+
+//The parent for the object created and the prototype for the constructor are equal.
---
+++ objects-property-descriptors.js
@@ -0,0 +1,48 @@
+//Properties have attributes attached to them and these attributes prevent
+//a property of being enumerated, for example.
+
+const course = {
+  id: 1,
+  price: 20,
+  name: "JS for beginners",
+  description: "The easiest way to learn JS",
+  author: "Marcos Herrera"
+}
+
+//Property descriptor
+let objectBase = Object.getPrototypeOf(course)
+let descriptor = Object.getOwnPropertyDescriptor(objectBase, 'toString')
+console.log(descriptor)
+
+//* configurable => it means we can delete this property if we want to.
+//* enumerable => when we iterate over an object, it'll define if we can see it or not.
+//* writable => we can override this method, change its implementation. 
+
+//Set attributes to your own properties
+Object.defineProperty(course, 'id', {
+  writable: false, //read only
+  configurable: false,
+  enumerable: false
+})
+Object.defineProperty(course, 'name', {
+  writable: false,
+  configurable: false
+
+})
+Object.defineProperty(course, 'price', {
+  writable: false
+})
+
+//TEST AREA//
+for(let key in course) {
+  console.log(key)
+} 
+//price, name, description, auhtor. Id does not appear, because enumerable is false.
+
+//* Changing Value
+course.name = "Node" //It won't change because writable is set to false.
+
+//* Delete Property
+delete course.id //It will be false and this property won't be deleted.
+
+
