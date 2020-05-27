31aa2d20b10cf513262bba42b52c2a7f89d6a66a
Cássia <cascrisbern@gmail.com>
2020-05-27 12:20:12 -0300

    new tests with jasmine  added

--- objects-prototype-prototypical-inheritance.js
+++ objects-prototype-prototypical-inheritance.js
@@ -17,6 +17,17 @@ Course.prototype.toString = function() {
  
 // ==== 
 
+function Tshirt(size, price, title) {
+  this.size = size
+  this.price = price
+  this.title = title
+}
+
+Tshirt.prototype = Object.create(Product.prototype)
+Tshirt.prototype.constructor = Tshirt
+
+// ====
+
 function Product(price, title, description) {
   this.price = price
   this.title = title
@@ -27,32 +38,27 @@ function Product(price, title, description) {
 }
 
 Product.prototype.priceToReais = function() {
-  const reaisPrice = 5.69
-  return (this.price * reaisPrice).toFixed(2)
+  const reaisRate = 5.69
+  const finalReaisPrice = this.price * reaisRate
+  return Math.round(finalReaisPrice * 100) / 100
 }
 
 Product.prototype.toString = function() {
-  return `Course "JS object" with price ${this.price} USD (${this.priceToReais()} reais)`
+  return `Course ${this.title} with price ${this.price} USD (${this.priceToReais()} reais)`
 }
 
 
 // new Course.prototype.constructor() is equal to new Course()
-// Because we reset the courseBase prototype to become productBase prototype.
-// So, if we want to create a new course object, the prototype will be the productBase one.
+// Because we reset the courseBase (or Course.prototype) 
+// to have the productBase (Product.prototype) as its parent.
+// So, if we want to create a new course object, the prototype will inherit from the productBase.
 // That is important to reset the constructor.
-
-function Tshirt(size) {
-  this.size = size
-}
-
-Tshirt.prototype = Object.create(Product.prototype)
-Tshirt.prototype.constructor = Tshirt
 
 const course = new Course(10, "Node", "Learn fast")
 const product = new Product(10, "Node", "Learn fast")
-const tshirt = new Tshirt('S')
+const tshirt = new Tshirt('S', 10, "Node")
 
 module.exports = {course, product, tshirt}
 
 
-console.log(module.exports.product.prototype)
+//console.log(module.exports.product.prototype)
--- spec/test-objects-prototypical-inheritance-spec.js
+++ spec/test-objects-prototypical-inheritance-spec.js
@@ -2,9 +2,15 @@ const objects = require('../objects-prototype-prototypical-inheritance')
 
 describe('Check prototypes', function() {
   it('checks if the price is converted correctly', function() {
-    expect(objects.course.price).toBe(10)
-})
-  it('checks if they have the same type', function() {
-    expect().toBe()
+    expect(objects.course.priceToReais()).toEqual(56.9)
+  })
+  it('checks if it is string', function() {
+    expect(objects.product.toString()).toEqual("Course Node with price 10 USD (56.9 reais)")
+  })
+  it('checks if the price is converted correctly to Tshirt objects', function () {
+    expect(objects.tshirt.priceToReais()).toEqual(56.9)
+  })
+  it('checks if it is string', function() {
+    expect(objects.tshirt.toString()).toEqual("Course Node with price 10 USD (56.9 reais)")
   })
 })
