b9397f01dd7328f30126d79a2f9684133773fba6
Cássia <cascrisbern@gmail.com>
2020-06-03 17:07:33 -0300

    first version mexins

--- objects-prototype-mixins.js
+++ objects-prototype-mixins.js
@@ -50,7 +50,7 @@ module.exports = {Product, Course, Tshirt}
 
 /*
 Object.assign({}, name, name)
-We use this to copy the properties and methods from one obhect to another.
+We use this to copy the properties and methods from one object to another.
 We pass an empty object as the target and pass one or more source objects. This way
 the empty object will end up being a combination of the two objects.
 You can also use that for constructors. Instead passing an empty object, you pass
