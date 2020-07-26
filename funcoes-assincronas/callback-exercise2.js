3585e32bc10f961a44b53375d879363717c36609
Cássia <cascrisbern@gmail.com>
2020-07-26 16:07:52 -0300

    exercising async code

--- funcoes-assincronas/callback-exercise2.js
+++ funcoes-assincronas/callback-exercise2.js
@@ -1,24 +1,20 @@
 function doubleNumber(num, num2, callback) {
   let x = Math.floor(Math.random() * 100 + 1 )
-  let result
-  
+
   setTimeout( function() {
     const result = (num * 2) + num2
     callback(result)
   },x)
-  
-  return result
 }
 
-let result
-result = doubleNumber(5, 0, function(result) {
+doubleNumber(5, 0, function(result) {
   console.log(result)
 
-  doubleNumber(12, result, function(result) {
-    console.log(result)
+  doubleNumber(12, result, function(result2) {
+    console.log(result2)
     
-    doubleNumber(2, result, function(result) {
-      console.log(result)
+    doubleNumber(2, result2, function(result3) {
+      console.log(result3)
     })
   })
 })
