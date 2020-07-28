f2acdce0c9c08a54ca2e0b64824dd42a569f3d39
Cássia <cascrisbern@gmail.com>
2020-07-28 09:56:54 -0300

    exercise mask

---
+++ mask/percent-number.js
@@ -0,0 +1,43 @@
+const Mask = {
+  apply(input, func) {
+    setTimeout(function () {
+      input.value = Mask[func](input.value)
+    }, 1)
+  },
+
+  formatBRL(value) {
+    value = value.replace(/\D/g, "")
+
+    return new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    }).format(value / 100)
+  },
+
+  formatPercentage(value) {
+    let divide = 10000
+    const arr = value.split(',')
+    if (arr[1]) {
+      const decimals = arr[1].split('%')[0].length
+      console.log(value, decimals)
+      if (decimals > 2)
+        divide = Math.pow(10, decimals + 2)
+    }
+
+    value = value.replace(/\D/g, "")
+
+    return new Intl.NumberFormat('pt-BR', {
+      style: 'percent',
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 4
+    }).format(value / divide)
+  },
+
+  formatCPF(value) {
+    value = value.replace(/\D/g, '').slice(0, 11)
+    value = value.replace(/^(\d{3})(\d)/g, '$1.$2')
+    value = value.replace(/(.\d{3})(\d)/g, '$1.$2')
+    value = value.replace(/(.\d{3})(\d)/g, '$1-$2')
+    return value
+  }
+}
