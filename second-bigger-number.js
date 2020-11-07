b9aa292b10b185480cd9172321c7c1f6e0d1bf09
Cássia <cascrisbern@gmail.com>
2020-11-06 21:17:13 -0300

    another way of finding second bigger number

--- second-bigger-number.js
+++ second-bigger-number.js
@@ -1,23 +1,44 @@
 function getSecondLargest(nums) {
-  // Complete the function
-  let maxNumber = 0;
-  let index;
+// FIRST WAY OF SOLVING THIS PROBLEM
+//   let maxNumber = 0;
+//   let oldMaxNumber;
+//   let index;
   
-  for(let i = 0; i < nums.length; i++) {
-      if( nums[i] > maxNumber ) {
-          maxNumber = nums[i]
-          index = i
-      } 
-      
-      if ( nums[i] === maxNumber ) {
-          nums.splice(i,1)
-      } 
-  }
+//   for(let i = 0; i < nums.length; i++) {
+//       if( nums[i] > maxNumber ) {
+//           oldMaxNumber = maxNumber
+//           maxNumber = nums[i]
+//           index = i
 
-nums.splice(index,1)
+//       } else if (nums[i] === maxNumber) {
+//           nums.splice(i,1)
+//       }    
+//   }
+// nums.splice(index,1)
+// //return Math.max(...nums)      
+// return oldMaxNumber 
 
-return Math.max(...nums)       
+// SECOND WAY OF SOLVING THIS PROBLEM
+let first = 0;
+let second = 0;
+
+for (let i = 0; i < nums.length; i++) {
+
+    if (nums[i] > first) {
+        second = first
+        first = nums[i]
+    }
+
+    if (nums[i] > second && nums[i] < first) {
+        second = nums[i]
+    }
+}
+
+return second;
   
 }
 
-console.log(getSecondLargest([2,3,6,6,5]))
+console.log(getSecondLargest([2,3,6,5,6])) //5
+console.log(getSecondLargest([9,6,6,1,2])) //6
+console.log(getSecondLargest([3,2,1,1,2,2,8,9])) //8
+console.log(getSecondLargest([4,5,7,9,0,2,6,9])) //7
